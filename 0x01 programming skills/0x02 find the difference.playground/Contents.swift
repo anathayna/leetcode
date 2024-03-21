138a0bcafe587738663b7a74b1a6d6fc94e57652
ana thayna franca <anathaynafranca@gmail.com>
2024-03-21 18:36:59 -0300

    :tada: start(leetcode): starting find the difference problem

---
+++ 0x01 programming skills/0x02 find the difference.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+class Solution {
+    func findTheDifference(_ s: String, _ t: String) -> Character {
+        let extraChar: Character = " "
+        
+        return extraChar
+    }
+}
+
+let solution = Solution()
+
+solution.findTheDifference("abcd", "abcde")
+//solution.findTheDifference("", "y")
