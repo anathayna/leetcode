96b7fecb66b2575188b10a5be78c961b04840da3
Ana Thayna França <anathaynafranca@gmail.com>
2025-07-29 21:53:43 -0300

    :open_book: study(leetcode): add two pointers solution

--- 0x02 problem list/977_squares_of_a_sorted_array.py
+++ 0x02 problem list/977_squares_of_a_sorted_array.py
@@ -5,4 +5,20 @@ class Solution:
             result.append(i*i)
             
         result.sort()
-        return result
+        return result # 5ms O(n log n)
+
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        result = [0] * len(nums)
+        left, right = 0, len(nums) - 1
+        pos = len(nums) - 1
+
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                result[pos] = nums[left] ** 2
+                left += 1
+            else:
+                result[pos] = nums[right] ** 2
+                right -= 1
+            pos -= 1
+
+        return result # 5ms O(n)
